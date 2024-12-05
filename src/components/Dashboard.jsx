1a8c21cd11563afd8bb5b533e2bd8ae8d2ae66fe
Nikolaos Kladis <nikcladis@gmail.com>
2024-12-05 19:29:24 +0200

    Added Search and SearchResults Components

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,13 @@
 import Card from "@components/Card";
+import Header from "@components/Header";
+
+import { mockCompanyDetails } from "@constants/mock";
 
 const Dashboard = () => {
   return (
     <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
-      <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1">
-        <Card>Hello</Card>
+      <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
+        <Header name={mockCompanyDetails.name} />
       </div>
       <div className="md:col-span-2 row-span-4">
         <Card>Chart</Card>
--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "src"),
       "@components": path.resolve(__dirname, "src", "components"),
+      "@constants": path.resolve(__dirname, "src", "constants"),
     },
   },
 });
