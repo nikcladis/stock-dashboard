a050555adff326037430663131f5af4b3b9d2aaa
Nikolaos Kladis <nikcladis@gmail.com>
2024-12-07 00:42:57 +0200

    Search dark theme functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import ThemeContext from "@contexts/ThemeContext";
 import { mockSearchResults } from "@constants/mock";
 import SearchResults from "@components/SearchResults";
 import { XIcon, SearchIcon } from "@heroicons/react/solid";
 
 const Search = () => {
+  const { darkMode } = useContext(ThemeContext);
+
   const [input, setInput] = useState("");
   const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
 
@@ -17,11 +20,15 @@ const Search = () => {
   };
 
   return (
-    <div className="flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200">
+    <div
+      className={`flex items-center my-4 border-2 rounded-md relative z-50 w-96 ${
+        darkMode ? "bg-gray-900 border-gray-800" : "bg-white border-neutral-200"
+      }`}
+    >
       <input
         type="text"
         value={input}
-        className="w-full px-4 py-2 focus:outline-none rounded-md"
+        className={`w-full px-4 py-2 focus:outline-none rounded-md ${darkMode ? "bg-gray-900" : null}`}
         placeholder="Search stock..."
         onChange={(event) => setInput(event.target.value)}
         onKeyUp={(event) => {
