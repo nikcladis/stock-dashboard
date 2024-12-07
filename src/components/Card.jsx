5020ae6a0a135bb9c7f40b68f68cfd94d67c0096
Nikolaos Kladis <nikcladis@gmail.com>
2024-12-07 17:02:28 +0200

    Fixed Card not changing to dark theme

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ children }) => {
 
   return (
     <div
-      className={`w-full h-full rounded-md relative p-8 border-2 bg-white border-neutral-200 ${
+      className={`w-full h-full rounded-md relative p-8 border-2 ${
         darkMode ? "bg-gray-900 border-gray-800" : "bg-white border-neutral-200"
       }`}
     >
